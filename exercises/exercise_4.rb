f3c420a2f6e2e75219d34b54a9e2646ac08a5973
Harrison2301 <harrisonwinters2301@gmail.com>
2019-07-09 22:43:01 +0000

    complete exercise 1

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,18 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+SurreyStore= Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel: false, womens_apparel: true)
+WhistlerStore = Store.create(name: "Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false)
+YaletownStore = Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+
+@mens_stores.each do |item|
+    puts "#{item.name} #{item.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: 0..1000000)
+
+@womens_stores.each do |item|
+    puts "#{item.name} #{item.annual_revenue}"
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum = Store.sum(:annual_revenue)
+puts sum
+
+puts sum/Store.count
+
+puts Store.where(annual_revenue: 1000000..10000000).count
